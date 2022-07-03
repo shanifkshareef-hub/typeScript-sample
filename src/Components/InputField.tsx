78314f3a2427fe79c05e46118d83b115868cb9bb
Shanif.k.Shareef <shanifshareef@gmail.com>
2022-07-03 22:28:20 +0530

    initial commit

---
+++ src/Components/InputField.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const InputField = () => {
+  return (
+    <div>
+        <div className="input">
+        <input value='' type="text" placeholder="🖊️ Add item..." />
+        <i className="fas fa-plus"></i>
+
+      </div>
+    </div>
+  )
+}
+
+export default InputField
